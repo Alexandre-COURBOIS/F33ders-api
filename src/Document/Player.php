865a3e4ea0a96a7d807af6f457674b8c826c3c2b
Pierre <pierre@halavent@gmail.com>
2021-09-24 17:00:37 +0200

    Setting property of Player

--- src/Document/Player.php
+++ src/Document/Player.php
@@ -2,6 +2,7 @@
 
 namespace App\Document;
 
+use DateTime;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
@@ -21,8 +22,114 @@ class Player
     private string $username;
     
     /**
-     * @MongoDB\String 
+     * @MongoDB\Object
      */
-    private $userHistory;
+    private Object $userHistory;
+
+    /**
+     * @MongoDB\Date
+     */
+    private $createdAt;
+
+    /**
+     * @MongoDB\Date
+     */
+    private $updatedAt;
+
+    /**
+     * @MongoDB\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    /**
+     * @MongoDB\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return Object
+     */
+    public function getUserHistory(): object
+    {
+        return $this->userHistory;
+    }
+
+    /**
+     * @param Object $userHistory
+     */
+    public function setUserHistory(object $userHistory): void
+    {
+        $this->userHistory = $userHistory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param mixed $updatedAt
+     */
+    public function setUpdatedAt($updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
 
 }
