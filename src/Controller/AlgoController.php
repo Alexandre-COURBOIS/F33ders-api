f7b7a5380763548cd10b815c7f586c1e6bee50a8
Pierre <pierre.halavent@gmail.com>
2021-11-19 11:32:48 +0100

    Find team from midlaner

--- src/Controller/AlgoController.php
+++ src/Controller/AlgoController.php
@@ -21,134 +21,248 @@ class AlgoController extends AbstractController
     private PlayerService $playerService;
     private SerializerService $serializerService;
     private DocumentManager $dm;
-    
+
     public function __construct(RiotApiService $riotApiService, PlayerService $playerService, SerializerService $serializerService, DocumentManager $dm)
     {
-         $this->riotApiService = $riotApiService;
-         $this->playerService = $playerService;
-         $this->serializerService = $serializerService;
-         $this->dm = $dm;
+        $this->riotApiService = $riotApiService;
+        $this->playerService = $playerService;
+        $this->serializerService = $serializerService;
+        $this->dm = $dm;
     }
 
-    public function findTeam($data)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Route("api/algo/find-team", name="algo_findteam")
+     */
+    public function findTeam(Request $request)
     {
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'];
+        $data = $this->process($username);
+        dump($data);
         $arrayLane = [
-          "top" => '',
-          "jungle" => '',
-          "mid" => '',
-          'adc' => '',
-          'support' => ''  
+            "top" => [],
+            "jungle" => [],
+            "mid" => [],
+            'adc' => [],
+            'support' => []
         ];
         $allPLayer = $this->playerService->getAllPlayer();
-        if($data['mainRole'] == 'ADC') {
-            //findSupport
-            foreach($allPLayer as $player) {
+        if ($data['mainRole'] == 'ADC') {
+            $arrayLane['adc'] = $data;
+            foreach ($allPLayer as $player) {
                 $searchPlayer = $this->process($player->getUsername());
-                if($searchPlayer['mainRole'] == "SUPPORT" || $searchPlayer['secondaryRole'] == "SUPPORT") {
-                    if($searchPlayer['agressivity'] == $data['agressivty']) {
+                if ($searchPlayer['mainRole'] == "SUPPORT") {
+
+                    if (!empty($arrayLane['top']) || !empty($arrayLane['jungle'])) {
+                        if ($arrayLane['top']['ccer'] == false || $arrayLane['jungle']['ccer'] == false) {
+                            if ($searchPlayer['agressivity'] == $data['agressivty'] && $searchPlayer['ccer'] == true) {
+                                $arrayLane['support'] = $searchPlayer;
+                            }
+                        } else {
+                            if ($searchPlayer['agressivity'] == $data['agressivty']) {
+                                $arrayLane['support'] = $searchPlayer;
+                            }
+                        }
+                    }
+                }
+            }
+        } elseif ($data['mainRole'] == 'SUPPORT') {
+            $arrayLane['support'] = $data;
+            foreach ($allPLayer as $player) {
+                $searchPlayer = $this->process($player->getUsername());
+                if ($searchPlayer['mainRole'] == "ADC") {
+                    if ($searchPlayer['agressivity'] == $data['agressivity']) {
                         $arrayLane['support'] = $searchPlayer;
                     }
                 }
             }
-        } elseif($data['mainRole'] == 'SUPPORT') {
-            foreach($allPLayer as $player) {
-                $searchPlayer = $this->process($player->getUsername());
-                if($searchPlayer['mainRole'] == "ADC" || $searchPlayer['secondaryRole'] == "ADC") {
-                    if($searchPlayer['agressivity'] == $data['agressivty']) {
-                        $arrayLane['support'] = $searchPlayer;
-                    }
-                }
-            }
-        } elseif($data['mainRole'] == 'JUNGLE') {
+        } elseif ($data['mainRole'] == 'JUNGLE') {
             //find mid
-        } elseif($data['mainRole'] == 'TOP') {
+            if (empty($arrayLane['mid'])) {
+                foreach ($allPLayer as $player) {
+                    $searchPlayer = $this->process($player->getUsername());
+                    if ($data['agressivity'] == $searchPlayer['agressivity']) {
+                        if ($data['ccer'] == false) {
+                            if ($searchPlayer['ccer'] == true) {
+                                $arrayLane['mid'] = $searchPlayer;
+                            }
+                        } else {
+                            $arrayLane['mid'] = $searchPlayer;
+                        }
+                    }
+                }
+            }
+
+        } elseif ($data['mainRole'] == 'MID') {
+            $arrayLane['mid'] = $data;
+            $arrayLane['mid']['username'] = $username;
+            for ($i = 0; $i < count($arrayLane); $i++) {
+                foreach ($allPLayer as $player) {
+                    $searchPlayer = $this->process($player->getUsername());
+                    if (empty($arrayLane['jungle'])) {
+                        if($searchPlayer['mainRole'] == 'JUNGLE') {
+                            if ($data['vulnerable'] == true) {
+                                if ($searchPlayer['agressivity'] == true) {
+                                    if ($data['ccer'] == false) {
+                                        if ($searchPlayer['ccer'] == true) {
+                                            $arrayLane['jungle'] = $searchPlayer;
+                                            break;
+                                        }
+                                    }
+                                }
+                            } elseif ($data['agressivity'] == $searchPlayer['agressivity']) {
+                                if ($data['ccer'] == false) {
+                                    if ($searchPlayer['ccer'] == true) {
+                                        $arrayLane['jungle'] = $searchPlayer;
+                                        break;
+                                    }
+                                } else {
+                                    $arrayLane['jungle'] = $searchPlayer;
+                                    break;
+                                }
+                            }
+                        }
+                    } elseif(empty($arrayLane['top'])) {
+                        if($searchPlayer['mainRole'] == 'TOP') {
+                            if ($arrayLane['jungle']['agressivity'] == true && $arrayLane['mid']['agressivity'] == true) {
+                                if ($searchPlayer['teamfighter'] == true) {
+                                    $arrayLane['top'] = $searchPlayer;
+                                    break;
+                                }
+                            } elseif($searchPlayer['moneyPlayer'] == true) {
+                                $arrayLane['top'] = $searchPlayer;
+                                break;
+                            }
+                        }
+                    } elseif(empty($arrayLane['support'])) {
+                        if($searchPlayer['mainRole'] == 'SUPPORT') {
+                            if(!$arrayLane['top']['ccer'] || !$arrayLane['mid']['ccer'] || !$arrayLane['jungle']['ccer']) {
+                                if($searchPlayer['ccer']) {
+                                    $arrayLane['support'] = $searchPlayer;
+                                    break;
+                                }
+                            }
+                        } else {
+                            $arrayLane['support'] = $searchPlayer;
+                            break;
+                        }
+                    } elseif(empty($arrayLane['adc'])) {
+                        if($searchPlayer['mainRole'] == 'ADC') {
+                            if($searchPlayer['agressivity'] == $arrayLane['support']['agressivity']) {
+                                $arrayLane['adc'] = $searchPlayer;
+                            }
+                        }
+                    }
+                }
+            }
+        } elseif ($data['mainRole'] == 'TOP') {
             // fill
-        } elseif($data['mainRole'] == 'MID') {
-            //find jgl
         }
+
+        dump($arrayLane);
+        return JsonResponse::fromJsonString($this->serializerService->SimpleSerializer($arrayLane, 'json'), Response::HTTP_OK);
     }
-    
-    /**
-     * @Route("api/algo/process", name="algo_getavgstats")
-     */
-    public function process($username): array
+
+
+    private function process($username): array
     {
 //        $datas = json_decode($request->getContent(), true);
-        
+
 //        dump($this->playerService->getAllPlayer());
-        
-        if(!empty($username)) {
+
+        if (!empty($username)) {
             $data = $this->getAvgStats($username);
             $role = $this->getRole($username);
 
-            if(!empty($role)) {
+            if (!empty($role['main'])) {
                 $player['mainRole'] = $role['main'];
+            }
+            if (!empty($role['secondary'])) {
                 $player['secondaryRole'] = $role['secondary'];
             }
-
-            if(($data['avgFb'] + $data['avgFba']) > 0.2) {
-                $player['agressivity'] = true;
+            if (isset($data['avgFb']) && isset($data['avgFba'])) {
+                if (($data['avgFb'] + $data['avgFba']) > 0.2) {
+                    $player['agressivity'] = true;
+                } else {
+                    $player['agressivity'] = false;
+                }
             } else {
                 $player['agressivity'] = false;
             }
 
-            if($data['avgDeaths'] > 3) {
+
+            if ($data['avgDeaths'] > 3) {
                 $player['vulnerable'] = true;
             } else {
                 $player['vulnerable'] = false;
             }
-            if(($data['avgKill'] + $data['avgAssists']) > 15) {
+            if (($data['avgKill'] + $data['avgAssists']) > 15) {
                 $player['teamfighter'] = true;
             } else {
                 $player['teamfighter'] = false;
             }
-            if($data['avgTimeCc'] > 20) {
-                $player['ccer'] = true;
+            if (isset($data['avgTimeCc'])) {
+                if ($data['avgTimeCc'] > 20) {
+                    $player['ccer'] = true;
+                } else {
+                    $player['ccer'] = false;
+                }
             } else {
                 $player['ccer'] = false;
             }
-            if($data['avgGold'] > 10000) {
-                $player['moneyPlayer'] = true;
+
+            if (isset($data['avgGold'])) {
+                if ($data['avgGold'] > 10000) {
+                    $player['moneyPlayer'] = true;
+                } else {
+                    $player['moneyPlayer'] = false;
+                }
             } else {
                 $player['moneyPlayer'] = false;
             }
 
-            return $player;   
+
+//             dump($player);
+
+            return $player;
         }
-        
+
     }
-    
+
     public function getRole($username): array
     {
         $historique = $this->riotApiService->getUserApi($username);
-        
+
 //        dump($historique);
 //        dump($this->playerService->matchPlayedWithChampions('Druxys'));
-        
+
         $tabRoles = [];
-        foreach($historique['matches'] as $match) {
-            if($match['role'] == "DUO_SUPPORT") {
+        foreach ($historique['matches'] as $match) {
+            if ($match['role'] == "DUO_SUPPORT") {
                 $tabRoles[] = "SUPPORT";
-            } elseif($match['role'] == "DUO_CARRY") {
+            } elseif ($match['role'] == "DUO_CARRY") {
                 $tabRoles[] = "ADC";
-            } elseif($match['lane'] !== "BOTTOM" && $match['lane'] !== "NONE") {
+            } elseif ($match['lane'] !== "BOTTOM" && $match['lane'] !== "NONE") {
                 $tabRoles[] = $match['lane'];
             }
         }
-        
+
         $occurences = array_count_values($tabRoles);
-        
+
         $mainRoles = [];
-        foreach($occurences as $key => $value) {
-            if($value == max($occurences)) {
-                if(!isset($mainRoles['main'])) {
+        foreach ($occurences as $key => $value) {
+            if ($value == max($occurences)) {
+                if (!isset($mainRoles['main'])) {
                     $mainRoles['main'] = $key;
                 } else {
                     $mainRoles['secondary'] = $key;
                 }
                 unset($occurences[$key]);
             }
-            if(count($mainRoles) == 2) {
+            if (count($mainRoles) == 2) {
                 break;
             }
         }
@@ -157,90 +271,108 @@ class AlgoController extends AbstractController
         return $mainRoles;
 
     }
-    
+
     public function getAvgStats($username): array
     {
         $allMatchPlayer = $this->dm->getRepository(Player::class)->findOneBy(['username' => $username]);
 //        dump($allMatchPlayer);
         $games = [];
-        foreach($allMatchPlayer->getUserHistory()[0]['matches'] as $match)
-        {
+        foreach ($allMatchPlayer->getUserHistory()[0]['matches'] as $match) {
 //            dump($match);
             $games[] = $this->dm->getRepository(Match::class)->findOneBy(['matchId' => $match['gameId']]);
         }
-        
+
 //        dump($games[0]);
         $userData = [];
         $i = 0;
-        foreach($games as $match) {
-            $idParticipant = $this->getParticipantId($match->getMatch()[12], 'Druxys');
-            
-            foreach($match->getMatch()[11] as $userGameInfo) {
-                if($userGameInfo['participantId'] == $idParticipant) {
-                    $userData[$i]['kills'] = $userGameInfo['stats']['kills'];
-                    $userData[$i]['deaths'] = $userGameInfo['stats']['deaths'];
-                    $userData[$i]['assists'] = $userGameInfo['stats']['assists'];
-                    $userData[$i]['firstBloodKill'] = $userGameInfo['stats']['firstBloodKill'];
-                    $userData[$i]['firstBloodAssist'] = $userGameInfo['stats']['firstBloodAssist'];
+        foreach ($games as $match) {
+            if (isset($match)) {
+                $idParticipant = $this->getParticipantId($match->getMatch()[12], $username);
+
+                foreach ($match->getMatch()[11] as $userGameInfo) {
+                    if ($userGameInfo['participantId'] == $idParticipant) {
+                        $userData[$i]['kills'] = $userGameInfo['stats']['kills'];
+                        $userData[$i]['deaths'] = $userGameInfo['stats']['deaths'];
+                        $userData[$i]['assists'] = $userGameInfo['stats']['assists'];
+                        $userData[$i]['firstBloodKill'] = $userGameInfo['stats']['firstBloodKill'];
+                        $userData[$i]['firstBloodAssist'] = $userGameInfo['stats']['firstBloodAssist'];
 //                    $userData[$i]['firstTowerKill'] = $userGameInfo['stats']['firstTowerKill'];
 //                    $userData[$i]['firstTowerAssist'] = $userGameInfo['stats']['firstTowerAssist'];
-                    $userData[$i]['visionScore'] = $userGameInfo['stats']['visionScore'];
-                    $userData[$i]['timeCCingOthers'] = $userGameInfo['stats']['timeCCingOthers'];
-                    $userData[$i]['goldEarned'] = $userGameInfo['stats']['goldEarned'];
-                    $userData[$i]['longestTimeSpentLiving'] = $userGameInfo['stats']['longestTimeSpentLiving'];
+                        $userData[$i]['visionScore'] = $userGameInfo['stats']['visionScore'];
+                        $userData[$i]['timeCCingOthers'] = $userGameInfo['stats']['timeCCingOthers'];
+                        $userData[$i]['goldEarned'] = $userGameInfo['stats']['goldEarned'];
+                        $userData[$i]['longestTimeSpentLiving'] = $userGameInfo['stats']['longestTimeSpentLiving'];
+                    }
                 }
+                $i++;
             }
-            $i++;
+
         }
 //        dump($userData);
-        
+
         $kills = [];
         $deaths = [];
         $assists = [];
-        foreach($userData as $data) {
+        foreach ($userData as $data) {
             $kills[] = $data['kills'];
             $deaths[] = $data['deaths'];
             $assists[] = $data['assists'];
             $fb[] = $data['firstBloodKill'];
-            $fba[]= $data['firstBloodAssist'];
+            $fba[] = $data['firstBloodAssist'];
             $gold[] = $data['goldEarned'];
             $longestTimeSpentLiving[] = $data['longestTimeSpentLiving'];
             $visionScore[] = $data['visionScore'];
             $timeCCingOthers[] = $data['timeCCingOthers'];
         }
-        
+
         $avgKills = $this->getAvg($kills);
         $avgDeaths = $this->getAvg($deaths);
         $avgAssists = $this->getAvg($assists);
-        $avgFb = $this->getAvg($fb);
-        $avgFba = $this->getAvg($fba);
-        $avgAlive = $this->getAvg($longestTimeSpentLiving);
-        $avgVisionScore = $this->getAvg($visionScore);
-        $avgTimeCc = $this->getAvg($timeCCingOthers);
-        $avgGold = $this->getAvg($gold);
-        
+        if (isset($fb)) {
+            $avgFb = $this->getAvg($fb);
+            $arr['avgFb'] = $avgFb;
+        }
+        if (isset($fba)) {
+            $avgFba = $this->getAvg($fba);
+            $arr['avgFba'] = $avgFba;
+        }
+        if (isset($longestTimeSpentLiving)) {
+            $avgAlive = $this->getAvg($longestTimeSpentLiving);
+            $arr['avgAlive'] = $avgAlive;
+        }
+        if (isset($visionScore)) {
+            $avgVisionScore = $this->getAvg($visionScore);
+            $arr['avgVisionScore'] = $avgVisionScore;
+        }
+        if (isset($timeCCingOthers)) {
+            $avgTimeCc = $this->getAvg($timeCCingOthers);
+            $arr['avgTimeCc'] = $avgTimeCc;
+        }
+        if (isset($gold)) {
+            $avgGold = $this->getAvg($gold);
+            $arr['avgGold'] = $avgGold;
+        }
+
+
         $arr['avgKill'] = $avgKills;
         $arr['avgDeaths'] = $avgDeaths;
         $arr['avgAssists'] = $avgAssists;
-        $arr['avgFb'] = $avgFb;
-        $arr['avgFba'] = $avgFba;
-        $arr['avgAlive'] = $avgAlive;
-        $arr['avgVisionScore'] = $avgVisionScore;
-        $arr['avgTimeCc'] = $avgTimeCc;
-        $arr['avgGold'] = $avgGold;
-        
+
         return $arr;
 //        return JsonResponse::fromJsonString($this->serializerService->SimpleSerializer($arr, 'json'), Response::HTTP_OK);
     }
-    
+
     private function getAvg($data)
     {
-        return array_sum($data)/count($data);
+        if (count($data) > 1) {
+            return array_sum($data) / count($data);
+        }
     }
-    
-    private function getParticipantId($arrParticipants, $summonerName) {
-        foreach($arrParticipants as $participant) {
-            if($participant['player']['summonerName'] == $summonerName) {
+
+    private function getParticipantId($arrParticipants, $summonerName)
+    {
+        foreach ($arrParticipants as $participant) {
+            if ($participant['player']['summonerName'] == $summonerName) {
                 return $participant['participantId'];
             }
         }
